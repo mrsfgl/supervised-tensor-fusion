c600d13d6b83dffd5b393c30986aa351bf50bcc5
Emre Sofuoglu <58656579+mrsfgl@users.noreply.github.com>
2021-02-02 12:18:41 -0500

    Add a demo for CMTF on sMRI and fMRI.

---
+++ sMRI_fMRI_demo.m
@@ -0,0 +1,72 @@
+
+addpath(genpath('M:\Documents\MATLAB\tboxunzipped\tensorlab'))
+
+path = '\\cifs.egr.msu.edu\research\sigimprg\Emre\databases\EEG-fMRI\EEG_FMRI';
+list_subjects = ls(path);
+list_subjects = list_subjects(3:end,:);
+n_subjects = size(list_subjects,1);
+
+F = cell(1,n_subjects);
+output = F;
+for i = 1:n_subjects
+    % For each subject extract sMRI and 3 auditory task data. Apply
+    % necessary reshaping operations such that factors match in size.
+    I = imresize3(niftiread([path,'\',list_subjects(i,:),'\anat\sub-0',...
+        num2str(i),'_anat_sub-0',num2str(i),'_T1w.nii.gz']),...
+        [256,256,150]);
+    imsMRI = cast(I,'single');
+    imfMRI = zeros(64,64,32,170,3);
+    for j=1:3
+        I = niftiread([path,'\',list_subjects(i,:),'\func\sub-0',num2str(i),...
+            '_task-auditoryoddballwithbuttonresponsetotargetstimuli_run-0',...
+            num2str(j),'_bold.nii.gz']);
+        imfMRI(:,:,:,:,j) = cast(I,'single');
+    end
+    % Learn factors for each subject. These factors are subject-specific.
+    % Hence they can be utilized in a CP-STM framework.
+    [F{i}, output{i}] = extractFactors(imsMRI, imfMRI);
+end
+
+function [F, output] = extractFactors(imsMRI, imfMRI)
+% [F, output] = extractFactors(imsMRI, imfMRI)
+% Extracts coupled and uncoupled factors of structural and functional MRI.
+R = 5;
+sz_s = size(imsMRI);
+sz_f = size(imfMRI);
+
+% Define model variables
+model = struct;
+model.variables.u1 = randn(sz_s(1),R);
+model.variables.u2 = randn(sz_s(2),R);
+model.variables.u3 = randn(sz_s(3),R);
+model.variables.v1 = randn(sz_f(4),R);
+model.variables.v2 = randn(sz_f(5),R);
+
+% Define subsampling matrices
+r_horizontal = ceil(sz_s(1)/sz_f(1));
+avM = kron(eye(sz_f(1)), ones(r_horizontal,1))/r_horizontal;
+r_vertical = ceil(sz_s(3)/sz_f(3));
+avH = kron(eye(sz_f(3)), ones(r_vertical,1))/r_vertical;
+avH = avH(1:sz_s(3),:);
+
+% Define factor relations to variables.
+model.factors.U1 = {'u1'};
+model.factors.U2 = {'u2'};
+model.factors.U3 = 'u3';
+model.factors.V1 = {'u1', @(z, task) struct_matvec(z,task,avM')};
+model.factors.V2 = {'u2', @(z, task) struct_matvec(z,task,avM')};
+model.factors.V3 = {'u3', @(z, task) struct_matvec(z,task,avH')};
+model.factors.V4 = 'v1';
+model.factors.V5 = 'v2';
+
+% Define tensors and their factors to be learned.
+model.factorizations.tensor1.data = imsMRI;
+model.factorizations.tensor1.cpd = {'U1','U2','U3'};
+
+model.factorizations.tensor2.data = imfMRI;
+model.factorizations.tensor2.cpd = {'V1','V2','V3','V4','V5'};
+
+% Optimize
+options.Display=5;
+[F, output] =  sdf_minf(model, options);
+end
