c1fc41a39c5227ad5534319d80680df3f4c9cc2c
Emre Sofuoglu <58656579+mrsfgl@users.noreply.github.com>
2021-04-02 18:48:33 +0300

    ACMTF with Soft coupling
    Added the factor extraction function for soft coupled ACMTF.

---
+++ extract_wsc_ACMTF.m
@@ -0,0 +1,26 @@
+function [Zhat, out]=extract_wsc_ACMTF(I1, I2, R, beta_cp, beta_pca)
+%EXTRACT_WSC_ACMTF
+%
+
+W{1}  = tt_create_missing_data_pattern(size(I1), 0);
+Z.object{1} = tensor(I1).*W{1};
+Z.miss{1} = W{1};
+W{2}  = tt_create_missing_data_pattern(size(I2), 0);
+Z.object{2} = tensor(I2).*W{2};
+Z.miss{2} = W{2};
+Z.modes = {[1,2,3],[4,3]};
+Z.size  = [size(I1),size(I2,1)];
+
+%% Fit ACMTF using one of the first-order optimization algorithms 
+options = ncg('defaults');
+options.Display ='final';
+options.MaxFuncEvals = 100000;
+options.MaxIters     = 1000;
+options.StopTol      = 1e-6;
+options.RelFuncTol   = 1e-6;
+
+init = 'random';
+% fit ACMTF-OPT
+[Zhat,~,out] = acmtf_opt_wsc(Z,R,'init',init,'alg_options',options, 'beta_cp', beta_cp, 'beta_pca', beta_pca); 
+
+end
